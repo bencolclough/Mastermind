a022f9fde7da38f3e7393c2195111f4d81033791
bencolclough <ben@tourdust.com>
2020-05-15 14:58:41 +0100

    allow player to be code maker or breaker

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,5 @@
 module RandomColor
-  def random_color
+  def self.random_color
     number = rand(5)
     case number
     when 0
@@ -18,20 +18,37 @@ module RandomColor
         "Error, incorrect random number generated"
     end
   end
-end
 
-class Mastermind
-  include RandomColor  
-
-  def generate_computer_code
+  def self.generate_computer_code
     [random_color,random_color,random_color,random_color]
   end
 
-  def get_guess
-    print "Enter Your Guess, seperate by space: "
+  def self.get_code 
+    print "Enter Your Secret Code, seperate by space: "
     gets.chomp.split(/\W+/)
   end
-  
+end
+
+class StartGame
+  include RandomColor
+
+  def choose_game
+    print "Welcome to Mastermind! Do you want break the code (b) or set the code (s): "
+    player_choice = gets.chomp
+    if player_choice == "b"
+      code = RandomColor.generate_computer_code
+      CodeBreaker.new.game(code)
+    elsif player_choice == "s"
+      code = RandomColor.get_code
+      CodeMaker.new.game(code)
+    else choose_game
+    end
+  end
+end
+
+class Mastermind
+  include RandomColor
+
   def check_perfect_matches(guess, code)
     matches = 0
     guess.each_with_index do |guess_color,guess_index|
@@ -39,7 +56,7 @@ class Mastermind
         matches += 1
       end
     end
-    p matches
+    return matches
   end
 
   def check_imperfect_matches(guess,code)
@@ -47,30 +64,33 @@ class Mastermind
     guess.uniq.each do |guess_color|
       matches += [code.count(guess_color), guess.count(guess_color)].min
     end
-    p matches
+    return matches
+  end
+end
+
+class CodeBreaker < Mastermind
+       
+  def get_guess 
+    print "Enter Your Guess, seperate by space: "
+    gets.chomp.split(/\W+/)
   end
 
+  def game(code)
+    perfect_matches = 0
+    imperfect_matches = 0
+    round = 0
 
-  def check_guess(guess, code, perfect_matches, imperfect_matches)  
-    # count perfect matches
-    guess.each_with_index do |guess_color,guess_index|
-      if guess_color == code[guess_index]
-        perfect_matches += 1
-      end
+    while round <= 12 && perfect_matches <= 4 do
+      break if perfect_matches == 4
+      guess = get_guess
+      perfect_matches = check_perfect_matches(guess,code)
+      imperfect_matches = check_imperfect_matches(guess,code) - perfect_matches # don't double count perfect matches
+      round += 1
+      show_guess_result(perfect_matches, imperfect_matches, round)
     end
-    
-    # count unique imperfect matches only
-    guess.uniq.each do |guess_color|
-        imperfect_matches += [code.count(guess_color), guess.count(guess_color)].min
-    end
-    
-    # don't double count perfect matches
-    imperfect_matches -= perfect_matches
-    p perfect_matches
-    p imperfect_matches
   end
 
-  def show_guess_result(perfect_matches, imperfect_matches)
+  def show_guess_result(perfect_matches, imperfect_matches, round) # will need one for each
     if imperfect_matches == 0 && perfect_matches == 0
       puts "Bummer, you got no matches"
     elsif perfect_matches > 0 && perfect_matches < 4
@@ -81,25 +101,43 @@ class Mastermind
       puts "You guessed #{perfect_matches} perfect matches and #{imperfect_matches} imperfect matches"
     end
   end
+end
 
-  def game
-    code = generate_computer_code
+class CodeMaker < Mastermind
+  include RandomColor
+
+  def computer_guess
+    [RandomColor.random_color,RandomColor.random_color,RandomColor.random_color,RandomColor.random_color]
+  end
+  
+  def game(code)
     perfect_matches = 0
     imperfect_matches = 0
     round = 0
-    p code
 
     while round <= 12 && perfect_matches <= 4 do
-      guess = get_guess
+      break if perfect_matches == 4
+      guess = computer_guess
       perfect_matches = check_perfect_matches(guess,code)
-      p perfect_matches
-      p check_imperfect_matches(guess,code)
-      imperfect_matches = check_imperfect_matches(guess,code) - perfect_matches
-      round += 1 # count number of rounds
-      show_guess_result(perfect_matches, imperfect_matches)
+      imperfect_matches = check_imperfect_matches(guess,code) - perfect_matches # don't double count perfect matches
+      round += 1
+      show_guess_result(perfect_matches, imperfect_matches, round)
+    end
+  end   
+  
+  
+  def show_guess_result(perfect_matches, imperfect_matches, round) # will need one for each
+    if imperfect_matches == 0 && perfect_matches == 0
+      puts "Round #{round}: The computer got no matches"
+    elsif perfect_matches > 0 && perfect_matches < 4
+      puts "Round #{round}: The computer guessed #{perfect_matches} perfect matches and #{imperfect_matches} imperfect matches"
+    elsif perfect_matches == 4
+      puts "Round #{round}: The computer cracked the code!"
+    else
+      puts "Round #{round}: The computer guessed #{perfect_matches} perfect matches and #{imperfect_matches} imperfect matches"
     end
   end
 end
 
-b = Mastermind.new
-b.game
+
+StartGame.new.choose_game
